c858fea50bb9e04ca8416aa8f7632bae092e21a7
Esteban Lopez <estlopez5@WKMZTD189076.global.publicisgroupe.net>
2023-08-30 13:33:23 -0500

    feature: automates dist invalidation

--- project/container/src/App.js
+++ project/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 const App = () => {
   return (
     <>
-      <h1>Hi container!</h1>
+      <h1>Hi container!!!!</h1>
       <hr />
       <MarketingApp />
     </>
